c7beeb9e78ba803102e329d2e9597bdf8c27b1aa
Hideaki Tai <hideaki.tai@gmail.com>
2020-09-29 17:19:19 +0900

    update namespace and compile flag based on new arx

--- MsgPack/Types.h
+++ MsgPack/Types.h
@@ -23,47 +23,15 @@
     #define INT5 INT5
 #endif
 
-#ifdef HT_SERIAL_MSGPACK_DISABLE_STL
-    #include "util/ArxContainer/ArxContainer.h"
-    #ifdef HT_SERIAL_MSGPACK_DISABLE_STL
-        #ifndef MSGPACK_MAX_PACKET_BYTE_SIZE
-            #define MSGPACK_MAX_PACKET_BYTE_SIZE 128
-        #endif // MSGPACK_MAX_PACKET_BYTE_SIZE
-        #ifndef MSGPACK_MAX_ARRAY_SIZE
-            #define MSGPACK_MAX_ARRAY_SIZE 8
-        #endif // MSGPACK_MAX_ARRAY_SIZE
-        #ifndef MSGPACK_MAX_MAP_SIZE
-            #define MSGPACK_MAX_MAP_SIZE 8
-        #endif // MSGPACK_MAX_MAP_SIZE
-        #ifndef MSGPACK_MAX_OBJECT_SIZE
-            #define MSGPACK_MAX_OBJECT_SIZE 32
-        #endif // MSGPACK_MAX_OBJECT_SIZE
-    #endif // HT_SERIAL_MSGPACK_DISABLE_STL
-#else
-    #include <vector>
-    #include <map>
-#endif // HT_SERIAL_MSGPACK_DISABLE_STL
-
 #include "util/ArxTypeTraits/ArxTypeTraits.h"
+#include "util/ArxContainer/ArxContainer.h"
 
 namespace ht {
 namespace serial {
 namespace msgpack {
 
-#ifdef HT_SERIAL_MSGPACK_DISABLE_STL
-    using idx_t = arx::vector<size_t, MSGPACK_MAX_OBJECT_SIZE>;
-    template <typename T, size_t N = MSGPACK_MAX_ARRAY_SIZE>
-    using arr_t = arx::vector<T, N>;
-    template <typename T, typename U, size_t N = MSGPACK_MAX_MAP_SIZE>
-    using map_t = arx::map<T, U, N>;
-    template <typename T, size_t N = MSGPACK_MAX_PACKET_BYTE_SIZE>
-    using bin_t = arx::vector<
-        typename std::enable_if<
-            std::is_same<T, uint8_t>::value || std::is_same<T, char>::value, T
-        >::type,
-        N
-    >;
-#else
+#if ARX_HAVE_LIBSTDCPLUSPLUS >= 201103L // Have libstdc++11
+
     using idx_t = std::vector<size_t>;
     template <typename T>
     using arr_t = std::vector<T>;
@@ -76,7 +44,37 @@ namespace msgpack {
         >::type,
         std::allocator<T>
     >;
-#endif
+
+#else // Do not have libstdc++11
+
+    #ifndef MSGPACK_MAX_PACKET_BYTE_SIZE
+        #define MSGPACK_MAX_PACKET_BYTE_SIZE 128
+    #endif // MSGPACK_MAX_PACKET_BYTE_SIZE
+    #ifndef MSGPACK_MAX_ARRAY_SIZE
+        #define MSGPACK_MAX_ARRAY_SIZE 8
+    #endif // MSGPACK_MAX_ARRAY_SIZE
+    #ifndef MSGPACK_MAX_MAP_SIZE
+        #define MSGPACK_MAX_MAP_SIZE 8
+    #endif // MSGPACK_MAX_MAP_SIZE
+    #ifndef MSGPACK_MAX_OBJECT_SIZE
+        #define MSGPACK_MAX_OBJECT_SIZE 32
+    #endif // MSGPACK_MAX_OBJECT_SIZE
+
+    using idx_t = arx::vector<size_t, MSGPACK_MAX_OBJECT_SIZE>;
+    template <typename T, size_t N = MSGPACK_MAX_ARRAY_SIZE>
+    using arr_t = arx::vector<T, N>;
+    template <typename T, typename U, size_t N = MSGPACK_MAX_MAP_SIZE>
+    using map_t = arx::map<T, U, N>;
+    template <typename T, size_t N = MSGPACK_MAX_PACKET_BYTE_SIZE>
+    using bin_t = arx::vector<
+        typename std::enable_if<
+            std::is_same<T, uint8_t>::value || std::is_same<T, char>::value, T
+        >::type,
+        N
+    >;
+
+#endif // Do not have libstdc++11
+
 #ifdef ARDUINO
     using str_t = String;
 #else
