a84af34ad802c76fca2fc3666b47063c96896194
Hideaki Tai <hideaki.tai@gmail.com>
2021-04-01 13:51:28 +0900

    add control scope for DebugLog

--- MsgPack.h
+++ MsgPack.h
@@ -2,8 +2,11 @@
 #ifndef HT_SERIAL_MSGPACK_H
 #define HT_SERIAL_MSGPACK_H
 
+#include "MsgPack/util/DebugLog/DebugLog.h"
 #ifndef MSGPACK_DEBUGLOG_ENABLE
-#define NDEBUG  // disable conversion warning
+#include "MsgPack/util/DebugLog/DebugLogEnable.h"
+#else
+#include "MsgPack/util/DebugLog/DebugLogDisable.h"
 #endif
 
 #include "MsgPack/Types.h"
@@ -11,5 +14,7 @@
 #include "MsgPack/Unpacker.h"
 
 namespace MsgPack = ht::serial::msgpack;
+
+#include "MsgPack/util/DebugLog/DebugLogRestoreState.h"
 
 #endif  // ARDUINOMSGPACK_H
