e76ec9989a936a2b8a2118be5ee34011f59955d3
Hideaki Tai <hideaki.tai@gmail.com>
2021-10-12 17:28:05 +0900

    add save_load_eeprom example

---
+++ examples/save_load_eeprom/save_load_eeprom.ino
@@ -0,0 +1,90 @@
+#define MSGPACK_DEBUGLOG_ENABLE
+#include <MsgPack.h>
+
+struct Meta {
+    int version;
+    String description;
+
+    MSGPACK_DEFINE(version, description);
+};
+
+struct Data {
+    int i;
+    float f;
+    String s;
+
+    MSGPACK_DEFINE(i, f, s);
+};
+
+struct MyConfig {
+    Meta meta;
+    Data data;
+
+    MSGPACK_DEFINE(meta, data);
+};
+
+void print_config(const MyConfig& config) {
+    Serial.print("version     : ");
+    Serial.println(config.meta.version);
+    Serial.print("description : ");
+    Serial.println(config.meta.description);
+    Serial.print("data (i)    : ");
+    Serial.println(config.data.i);
+    Serial.print("data (f)    : ");
+    Serial.println(config.data.f);
+    Serial.print("data (s)    : ");
+    Serial.println(config.data.s);
+}
+
+void setup() {
+    Serial.begin(115200);
+    EEPROM.begin();
+    delay(2000);
+
+    MyConfig config;
+
+    // load current config
+    if (!MsgPack::eeprom::load(config)) {
+        Serial.println("No configuration found on EEPROM");
+    } else {
+        Serial.println("Found configuration on EEPROM:");
+        print_config(config);
+    }
+
+    Serial.println();
+    Serial.println("If you want to save new configuration, press any key...");
+
+    while (!Serial.available()) {
+        ;
+    }
+
+    // clear
+    MsgPack::eeprom::clear(config);
+    // or clear manually with size
+    // const size_t size = MsgPack::estimate_size(config);
+    // MsgPack::eeprom::clear_size(size);
+
+    // change config values
+    randomSeed(analogRead(A0));  // change random seed every time
+    config.meta.version = random(0, 100);
+    config.meta.description = String("my setting on ") + __TIME__;
+    config.data.i = random(100, 200);
+    config.data.f = random(100, 200);
+    config.data.s = String("random ") + random(100, 200);
+
+    // save
+    MsgPack::eeprom::save(config);
+
+    // load
+    if (!MsgPack::eeprom::load(config)) {
+        Serial.println("[ERROR] Saving configuration to EEPROM failed");
+    } else {
+        Serial.println("Saving configuration to EEPROM success");
+        print_config(config);
+    }
+
+    EEPROM.end();
+    Serial.println("===== FINISH =====");
+}
+
+void loop() {}
